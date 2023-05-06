53daec6a3550c899535b1d8d55acdc1803a3dc70
AlmazDD <iheardthatwas@gmail.com>
2023-05-06 09:16:34 +0600

    commit

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -56,13 +56,13 @@ export function MainNavigation() {
                       href="/chat"
                       className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
                     >
-                      Table
+                      Chat
                     </Link>
                     <Link
                       href="/timer"
                       className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
                     >
-                      Table
+                      Timer
                     </Link>
                   </div>
                 </div>
